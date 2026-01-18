05d54a2a7a75004223e21cdad452b2f5fc393f4c
Mirampali sravanthi <mirampallisravanthi@gmail.com>
2026-01-18 18:35:46 +0530

    Initial commit

---
+++ pizzas.py
@@ -0,0 +1,15 @@
+pizza = int(input("enter the number of pizzas: "))
+puffs = int(input("enter the number of puffs: "))
+cooldrinks = int(input("enter the cooldrinks: "))
+
+pizza_price = 100
+puff_price = 20
+cooldrink_price =10
+
+total = (pizza * pizza_price)+ (puffs * puff_price) + (cooldrinks * cooldrink_price)
+print("\n bill details")
+print("no of pizza:", pizza)
+print("no of puffs:",puffs)
+print("no of cooldrinks:",cooldrinks)
+print("total",total)
+print("enjoy the show")
